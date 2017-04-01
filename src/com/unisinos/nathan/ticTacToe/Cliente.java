8737941d5c7ae7ba332ff38d6c4635b06cddf37c
Nathan Silveira <nsschneider1@gmail.com>
2017-04-01 14:58:02 -0300

    Add rules on Client

--- src/com/unisinos/nathan/ticTacToe/Cliente.java
+++ src/com/unisinos/nathan/ticTacToe/Cliente.java
@@ -12,6 +12,13 @@ public class Cliente {
 		int socket = argv.length > 0 ? Integer.parseInt(argv[0]) : 6789;
 		
 		BufferedReader doUsuario = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("****COMO JOGAR****");
+		System.out.println("Na sua vez, digite sua jogada no seguinte formato:");
+		System.out.println("<jogada>,<coordenada_x>,<coordenada_y>");
+		System.out.println("Valores de jogada válidos: X e O maiúsculos");
+		System.out.println("Valores de coordenadas válidos: de 0 a 2, desde que os espaços que estejam vazios");
+		System.out.println("O jogador da porta 6789 começa primeiro");
+		System.out.println("Para perguntar ao servidor o status atual do jogo, aperte enter sem passar nenhum texto");
 
 		
 		
